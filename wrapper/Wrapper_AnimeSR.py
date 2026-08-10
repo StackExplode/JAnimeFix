c01e18b8024011c53763b9d368a1237eb3548920
StackExplode <785532231@qq.com>
2026-08-10 23:13:46 +0800

    fix path bug

--- wrapper/Wrapper_AnimeSR.py
+++ wrapper/Wrapper_AnimeSR.py
@@ -4,8 +4,7 @@ import sys
 import torchvision.transforms.functional as TF
 sys.modules['torchvision.transforms.functional_tensor'] = TF
 
-CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
-ANIMESR_REPO_DIR = os.path.join(CURRENT_DIR, 'driver/AnimeSR')
+ANIMESR_REPO_DIR = os.path.join(os.getcwd(), 'driver/AnimeSR')
 if ANIMESR_REPO_DIR not in sys.path:
 	sys.path.insert(0, ANIMESR_REPO_DIR)
 
