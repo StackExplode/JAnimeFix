0b77d68a73c71cce46876d4f2507348103c4e2de
StackExplode <785532231@qq.com>
2026-08-10 23:15:50 +0800

    fix class name misspell

--- wrapper/Wrapper_AnimeSR.py
+++ wrapper/Wrapper_AnimeSR.py
@@ -17,7 +17,7 @@ from .WrapperBase import WrapperBase
 # 导入官方提供的工具函数
 from animesr.utils.inference_base import get_base_argument_parser, get_inference_model
 
-class WrapperAnimeSR(WrapperBase):
+class Wrapper_AnimeSR(WrapperBase):
 	def __init__(self, globaljson, json):
 		super().__init__(globaljson, json)
 		self.device = torch.device(self.GetGlobalSetting("device", "cuda:0"))
